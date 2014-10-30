bf98a76dc276f233b3b487f343d071a18a87afff
Sechaba Mothobi <Sechaba.Mothobi@enca.com>
2014-10-30 15:36:11 +0200

    Project Init

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+// File: /js/app.js
+// 'jquery' returns the jQuery object into '$'
+// 'bootstrap' does not return an object. Must appear at the end
+
+require(["bootstrap/carousel"], function(carousel) {});
+require(["bootstrap/collapse"], function(collapse) {});
+require(["bootstrap/dropdown"], function(dropdown) {});
+require(["bootstrap/transition"], function(transition) {});
